4747a5b4206a475bd06d31744ca44a9b4034152a
Nikola Naumov <bat6oko@abv.bg>
2017-05-20 17:07:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(event) {
   // pre cache a load of stuff:
   event.waitUntil(
-    caches.open('myapp-static-v2').then(function(cache) {
+    caches.open('myapp-static-v3').then(function(cache) {
       return cache.addAll([
         'custom-style.css',
         'custom-style.css',
@@ -9,10 +9,10 @@ self.addEventListener('install', function(event) {
         'favicon.ico',
         'index.html',
         'logo-site.png',
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-        'https://code.jquery.com/jquery-3.1.1.slim.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js',
-        'https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js'
+        'lib/font-awesome.min.css',
+        'lib/jquery-3.1.1.slim.min.js',
+        'lib/tether.min.js',
+        'lib/bootstrap-4.0.0-alpha.6.min.js'
       ]);
     })
   )
